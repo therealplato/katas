8c48cf5bcb8a8e721189ded277d2efe4d39b06d1
therealplato <therealplato@gmail.com>
2016-07-26 21:43:30 -0400

    add types

--- main.go
+++ main.go
@@ -10,26 +10,45 @@ import (
 	"github.com/pkg/profile"
 )
 
-// N is size of list (up to 10^7)
-var N int
-
-// M is number of ops (up to 2*10^5)
-var M int
+// // N is size of list (up to 10^7)
+// var N int
+//
+// // M is number of ops (up to 2*10^5)
+// var M int
 
 // a is first index (1 indexed)
 // b is second index (1 indexed)
 // k is amount to add this operation (up to 10^9)
-var a, b, k int
+// var a, b, k int
+
+type Cfg struct {
+	N int
+	M int
+}
+
+var cfg Cfg
+
+type Transform struct {
+	a int
+	b int
+	k int
+}
+
+type State []int
+
+type TransformResult []int
 
 func main() {
 	defer profile.Start(profile.CPUProfile).Stop()
-	var state []int
+	var state State
 	scanner := bufio.NewScanner(os.Stdin)
 	if scanner.Scan() {
 		state = handleFirstLine(scanner.Text())
 	}
 	for scanner.Scan() {
-		state = handleLine(state, scanner.Text())
+		transform := handleLine(state, scanner.Text())
+		diff := calcTransform(state, transform)
+		state = applyTransform(state, diff)
 	}
 	if err := scanner.Err(); err != nil {
 		panic(err)
@@ -37,15 +56,16 @@ func main() {
 	fmt.Println(max(state))
 }
 
-func handleFirstLine(s string) []int {
+func handleFirstLine(s string) State {
 	first := strings.Split(s, " ")
 	if len(first) != 2 {
 		panic("wrong len of first line")
 	}
-	N, _ = strconv.Atoi(first[0])
-	M, _ = strconv.Atoi(first[1])
+
+	cfg.N, _ = strconv.Atoi(first[0])
+	cfg.M, _ = strconv.Atoi(first[1])
 	// initialize zeroed array:
-	return make([]int, N)
+	return State(make([]int, cfg.N))
 	// var i int
 	// for i = 0; i < N; i++ {
 	// 	state = append(state, 0)
@@ -54,14 +74,27 @@ func handleFirstLine(s string) []int {
 }
 
 // handleLine parses one input transformation line and updates state0
-func handleLine(state []int, s string) []int {
+func handleLine(state State, s string) Transform {
 	nums := strings.Split(s, " ")
 	// a and b are one-indexed indices
-	a, _ = strconv.Atoi(nums[0])
-	b, _ = strconv.Atoi(nums[1])
+	a, _ := strconv.Atoi(nums[0])
+	b, _ := strconv.Atoi(nums[1])
 	// k is how much this transform adds to elements of state with indices a..b inclusive
-	k, _ = strconv.Atoi(nums[2])
-	return updateState(state, a-1, b-1, k)
+	k, _ := strconv.Atoi(nums[2])
+	return Transform{
+		a: a,
+		b: b,
+		k: k,
+	}
+	// updateState(state, a-1, b-1, k)
+}
+
+func calcTransform(state State, t Transform) State {
+	return updateState(state, t.a-1, t.b-1, t.k)
+}
+
+func applyTransform(state State, t State) State {
+	return t
 }
 
 // updateState adds k to state, indices i through j inclusive
