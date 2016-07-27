9f44a04d973f2f357b4c94459144424a4dad3fd2
therealplato <therealplato@gmail.com>
2016-07-27 07:05:00 -0400

    work function and diff

--- async.go
+++ async.go
@@ -2,26 +2,51 @@ package main
 
 // async.go runs portions of the task asynchronously
 
-var Zeroed State
-
+// Job describes a task for the worker
+// This job is to generate a Diff containing []int of width b-a containing values k
 type Job struct {
-	State     *State
-	Transform Transform
+	t Transform
+}
+
+// Diff is a fragment of a State slice affected by this transform
+type Diff struct {
+	t Transform
+	s State // not full width
+	l int
 }
 
 // Worker is spawned by a goroutine to help main
 type Worker struct {
-	input   <-chan Job
-	scratch State
-	output  chan<- State
+	input  <-chan Job
+	output chan<- Diff
 }
 
 // Work loops through input
 func (w *Worker) Work() {
+	var in Job
 	for {
-		in := <-w.input
-		copy(w.scratch, *in.State)
-		applyTransform(w.scratch, in.Transform)
-		output <- w.scratch
+		// wait for job:
+		in = <-w.input
+		// calculate how long the diff is:
+		l := in.t.b - in.t.a + 1 // a,b one indexed and inclusive
+		// make a too-big scratch pad:
+		scratch := make([]int, 2*l)
+		i := 0 // zero index left of populated
+		j := 1 // zero index after populated k's
+		z := 1 // width of this copy
+		// populate scratch with k's longer than diff:
+		scratch[0] = in.t.k
+		for j <= l {
+			copy(scratch[i+z:j+z], scratch[i:j])
+			i += z
+			j += z
+			z += z
+		}
+		// return diff:
+		w.output <- Diff{
+			s: scratch[:l],
+			t: in.t,
+			l: l,
+		}
 	}
 }
--- main.go
+++ main.go
@@ -21,55 +21,82 @@ import (
 // k is amount to add this operation (up to 10^9)
 // var a, b, k int
 
+// Cfg stores max width and max transforms
 type Cfg struct {
 	N int
 	M int
 }
 
-var cfg Cfg
+var complete int
 
+// Transform describes how to change the state for this line
 type Transform struct {
 	a int
 	b int
 	k int
 }
 
+// State is the full row, up to len 10e9
 type State []int
-type TransformResult []int
+
+var state State
+var cfg Cfg
 
 func main() {
 	defer profile.Start(profile.CPUProfile).Stop()
-	var state State
 	scanner := bufio.NewScanner(os.Stdin)
 	if scanner.Scan() {
-		state = handleFirstLine(scanner.Text())
+		// create zeroed state of width M:
+		state, cfg = handleFirstLine(scanner.Text())
 	}
-	chan queue Job
-	chan diffs State
-	w := &Worker{
-		input: queue,
 
+	queue := make(chan Job)
+	diffs := make(chan Diff)
+	w := &Worker{
+		input:  queue,
+		output: diffs,
+	}
+	go w.Work()
+	go setup(scanner, queue)
+	go output(diffs, cfg)
+}
+
+func setup(scanner *bufio.Scanner, queue chan Job) {
 	for scanner.Scan() {
-		transform := handleLine(state, scanner.Text())
-		diff := calcTransform(state, transform)
-		state = applyTransform(state, diff)
+		transform := handleLine(scanner.Text())
+		queue <- Job{
+			t: transform,
+		}
 	}
 	if err := scanner.Err(); err != nil {
 		panic(err)
 	}
+}
+func output(diffs chan Diff, cfg Cfg) {
+	for {
+		d := <-diffs
+		// diff := calcTransform(state, transform)
+		state = applyTransform(state, d)
+		complete++
+		if complete == cfg.N {
+			break
+		}
+	}
 	fmt.Println(max(state))
 }
 
-func handleFirstLine(s string) State {
+func handleFirstLine(s string) (State, Cfg) {
 	first := strings.Split(s, " ")
 	if len(first) != 2 {
 		panic("wrong len of first line")
 	}
 
-	cfg.N, _ = strconv.Atoi(first[0])
-	cfg.M, _ = strconv.Atoi(first[1])
+	c := Cfg{}
+
+	c.N, _ = strconv.Atoi(first[0])
+	c.M, _ = strconv.Atoi(first[1])
 	// initialize zeroed array:
-	return State(make([]int, cfg.N))
+	return State(make([]int, cfg.N)), c
 	// var i int
 	// for i = 0; i < N; i++ {
 	// 	state = append(state, 0)
@@ -78,7 +105,7 @@ func handleFirstLine(s string) State {
 }
 
 // handleLine parses one input transformation line and updates state0
-func handleLine(state State, s string) Transform {
+func handleLine(s string) Transform {
 	nums := strings.Split(s, " ")
 	// a and b are one-indexed indices
 	a, _ := strconv.Atoi(nums[0])
@@ -90,33 +117,37 @@ func handleLine(state State, s string) Transform {
 		b: b,
 		k: k,
 	}
-	// updateState(state, a-1, b-1, k)
 }
 
-func calcTransform(state State, t Transform) State {
-	return updateState(state, t.a-1, t.b-1, t.k)
-}
-
-func applyTransform(state State, t State) State {
-	return t
-}
-
-// updateState adds k to state, indices i through j inclusive
-func updateState(state []int, i, j, k int) []int {
-	// for x, v := range state {
-	// 	if int(x) >= i && int(x) <= j {
-	// 		state[int(x)] = v + k
-	// 	}
-	// }
-
-	// i is zero indexed and flags the leftmost state element to bump
-	for y, z := range state[i : j+1] {
-		// y is zero index within the slice of state
-		// add i to get index within state
-		state[y+i] = z + k
+func applyTransform(state State, d Diff) State {
+	fmt.Printf("t: %+v", d.t)
+	i := d.t.a - 1 // state index
+	var j int      // diff index
+	for i < d.l {
+		state[i] += d.s[j]
+		i++
+		j++
 	}
 	return state
 }
+
+//
+// // updateState adds k to state, indices i through j inclusive
+// func updateState(state []int, i, j, k int) []int {
+// 	// for x, v := range state {
+// 	// 	if int(x) >= i && int(x) <= j {
+// 	// 		state[int(x)] = v + k
+// 	// 	}
+// 	// }
+//
+// 	// i is zero indexed and flags the leftmost state element to bump
+// 	for y, z := range state[i : j+1] {
+// 		// y is zero index within the slice of state
+// 		// add i to get index within state
+// 		state[y+i] = z + k
+// 	}
+// 	return state
+// }
 
 func max(state []int) int {
 	var tmp int
