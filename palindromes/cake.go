109d00e22bfbb045431199ed0fa41624d3f842af
Isaac Rogers <therealplato@gmail.com>
2018-03-10 14:16:41 +1300

    palindromes: link layers

--- palindromes/cake.go
+++ palindromes/cake.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"log"
+)
+
 type layer struct {
 	i     int // left index
 	j     int // right index
@@ -19,46 +24,38 @@ func (l layer) String() string {
 }
 
 func build(chars []byte) (bottom *layer) {
+	log.Println("building " + string(chars))
 	// we'll destructively modify slice header:
 	c := make([]byte, len(chars), len(chars))
 	copy(c, chars)
 
 	// layers[0] = bottom
-	layers := make([]layer, 0)
+	layers := make([]*layer, 0)
 
 	i := 0
-	j := len(c) - 1
-	for len(c) > 0 {
-		lay := layer{
+	j := len(c)
+	for i <= j {
+		c = chars[i:j]
+		j--
+		fmt.Printf("iterating layer, input: %s\n", string(c))
+		lay := &layer{
 			i:     i,
 			j:     j,
 			chars: chars,
 		}
+		i++
 		if len(layers) > 0 {
-			lay.down = &(layers[len(layers)])
-			(layers[len(layers)]).up = &lay
+			top := layers[len(layers)-1]
+			lay.down = top
+			top.up = lay
 		}
 		layers = append(layers, lay)
-		if len(c) <= 1 {
-			break
-		}
-		i++
-		j--
-
-		c = c[i:j]
 	}
 	if len(layers) == 0 {
 		return nil
 	}
-	return &layers[0]
+	return layers[0]
 }
-
-// func edges(c []byte) (l, r byte) {
-// 	if len(c) == 1 {
-// 		return c[0], c[0]
-// 	}
-// 	return c[0], c[len(c)]
-// }
 
 func spaceFill(bb []byte) {
 	for i := range bb {
